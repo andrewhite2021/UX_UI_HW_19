fe3857da3397ea279388f774bac0aaf3d716216c
andrewhite2021 <astefwhite@gmail.com>
2022-01-27 02:03:21 +1100

    Adjustments to UI interactions and basic formating changes
    Adjusted Navbar item spacing, added Resume download description, added a loader animation.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+/*preloader*/
+function fade() {
+    $('.pageLoader').fadeOut("slow", "linear");
+}
+setTimeout(fade, 2900);
 /*responsive nav drop down slide toggle*/
 console.log("your indexe.js file is loaded correctly!");
 
