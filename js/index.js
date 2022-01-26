2cd82240acd8639d08bc0a4cd149748ae9255ec0
andrewhite2021 <astefwhite@gmail.com>
2022-01-26 11:10:40 +1100

    jQuery and keyframe animations added

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/*responsive nav drop down slide toggle*/
+console.log("your indexe.js file is loaded correctly!");
+
+$("#hamburger").on("click", function(){
+    $("#dropdown").slideToggle();
+})
+
---
+++ js/test.js
@@ -0,0 +1,4 @@
+$("#hamburger").on("click", function(){
+    $("dropdown").slideToggle();
+});
+console.log("what is going on here");
