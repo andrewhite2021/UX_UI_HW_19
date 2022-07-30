9193fe856a3fe7289a66a1ac7f21d5c11f1d697e
andrewhite2021 <astefwhite@gmail.com>
2022-07-30 22:21:25 +1000

    New menu code page added

--- js/index.js
+++ js/index.js
@@ -4,9 +4,28 @@ function fade() {
 }
 setTimeout(fade, 2900);
 /*responsive nav drop down slide toggle*/
-console.log("your indexe.js file is loaded correctly!");
+/*console.log("your indexe.js file is loaded correctly!");*/
 
-$("#hamburger").on("click", function(){
+/*$("#hamburger").on("click", function(){
     $("#dropdown").slideToggle();
 })
+*/
 
+//responsive navbar toggle//
+
+const navBar = document.querySelector(".navBar");
+const navToggle = document.querySelector(".mobile-Toggle-Hamburger");
+
+navToggle.addEventListener('click', () => {
+    const visibility = navBar.getAttribute("data-visible");
+
+    if (visibility === "false") {
+        navBar.setAttribute("data-visible", true);
+        navToggle.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else if (visibility === "true") {
+        navBar.setAttribute("data-visible", false);
+        navToggle.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+})
